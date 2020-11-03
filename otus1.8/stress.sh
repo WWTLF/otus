b4bd917015535f99d073274a7b3adf2a0a89fa11
Ершов Борис Владимирович <BVErshov.SBT@sberbank.ru>
2020-11-03 10:19:18 +0300

    new file:   NGINX_dash_board.png  	modified:   grafana-nginx-configmap.yaml

--- otus1.8/stress.sh
+++ otus1.8/stress.sh
@@ -1,5 +1,5 @@
 while true; 
 do 
-ab -n 50 -c 5 http://arch.homework/otusapp/borisershov/api/v1/user/393; 
+ab -n 50 -c 5 http://arch.homework/otusapp/borisershov/api/v1/user/979; 
 sleep 3; 
 done
